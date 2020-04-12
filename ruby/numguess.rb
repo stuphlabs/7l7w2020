0c1ebddbb192a1743f25d3a2baf25781f9c59ed9
Austin Gossmeyer <austin.gossmeyer+git@gmail.com>
2020-04-12 16:55:33 -0500

    corrected indentation of loop.

--- ruby/numguess.rb
+++ ruby/numguess.rb
@@ -5,16 +5,16 @@ randnum = rand(100)
 
 #read a number from cli
 loop do
-guess = gets
+	guess = gets
 
-#compare to random number
-if guess.to_i == randnum
-	break
-elsif guess.to_i > randnum
-	puts "You guessed too high."
-else
-	puts "You guessed too low."
-end
+	#compare to random number
+	if guess.to_i == randnum
+		break
+	elsif guess.to_i > randnum
+		puts "You guessed too high."
+	else
+		puts "You guessed too low."
+	end
 
 end
 
